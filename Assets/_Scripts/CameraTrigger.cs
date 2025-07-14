30312e70e71e9b942e86af1cb4b3d68dd88e626d
Sanchay Sahay <grindinforvalore@gmail.com>
2025-07-14 10:10:26 +0530

    Revert "Reapply "water reflection added to testlevel3""
    This reverts commit 6080fed76cf4341c9677e57001aa9cab8be16e49.

--- Assets/_Scripts/CameraTrigger.cs
+++ Assets/_Scripts/CameraTrigger.cs
@@ -4,34 +4,16 @@ public class CameraTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        if (collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player"))
         {
-            if (this.gameObject.name == "LakeCamTrigger")
-            {
-                CameraChanger.instance.LakePuzzleView();
-                return;
-            }
             CameraChanger.instance.ZoomIn();
-            Debug.Log("zooming in");
         }
-
-        
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-
-            if (this.gameObject.name == "LakeCamTrigger")
-            {
-                CameraChanger.instance.LakePuzzleView();
-                return;
-            }
-
             CameraChanger.instance.ZoomIn();
-            Debug.Log("zooming zoomed in");
-
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -39,8 +21,6 @@ public class CameraTrigger : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             CameraChanger.instance.ZoomOut();
-            Debug.Log("zoomed out");
-
         }
     }
 
