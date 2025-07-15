47fb5ebf492a7386bee24c8dc2363b1ee2138041
Sanchay Sahay <grindinforvalore@gmail.com>
2025-07-15 09:12:53 +0530

    tv + expressions

---
+++ Assets/_Scripts/ExpressionCHanger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExpressionCHanger : MonoBehaviour
+{
+    Slider intensity;
+    [SerializeField] Sprite expressionSad, expressionDepressed, expressionNeutral;
+    [SerializeField] UnityEngine.UI.Image Expression;
+    public void ChangeExpression()
+    {
+        if (intensity.value > 7)
+        {
+            Expression.sprite = expressionNeutral;
+        }
+        else if (intensity.value < 7 && intensity.value > 3)
+        {
+            Expression.sprite = expressionSad;
+        }
+        else
+            Expression.sprite = expressionDepressed;
+
+    }
+}
