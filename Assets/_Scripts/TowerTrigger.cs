278e93485037a2f069c3c336e96d75bba413eae2
Rudraneil <rudraneil123sarkar@gmail.com>
2025-07-15 11:09:24 +0530

    Level 2 assets Imported

--- Assets/_Scripts/TowerTrigger.cs
+++ Assets/_Scripts/TowerTrigger.cs
@@ -5,6 +5,9 @@ public class TowerTrigger : MonoBehaviour
 {
     public UnityEvent Event;
     public UnityEvent Exit;
+
+    public Transform DesiredDestination;
+    public Transform PlayerPos;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
@@ -18,5 +21,14 @@ public class TowerTrigger : MonoBehaviour
         {
             Exit?.Invoke();
         }
+    }
+
+    public void Teleport()
+    {
+        Invoke("TeleportPlayer", 1);
+    }
+     void TeleportPlayer()
+    {
+        PlayerPos.position = DesiredDestination.position;
     }
 }
