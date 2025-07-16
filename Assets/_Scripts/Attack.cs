11e7ed06f65c8e34c1ec20f92905503bbebcf377
Rudraneil <rudraneil123sarkar@gmail.com>
2025-07-16 13:06:00 +0530

    Final Changes

--- Assets/_Scripts/Attack.cs
+++ Assets/_Scripts/Attack.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Attack : MonoBehaviour
 {
     public int DamageAmount = 50;
+
+    public bool canRespawn = false;
+
+    public Transform RespawnPoint;
+    public UnityEvent Event;
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            LanternController controller = collision.gameObject.GetComponentInChildren<LanternController>();
-            controller.TakeDamage(DamageAmount);
-
-
+            Transform playerTransform = collision.gameObject.transform;
+            if(canRespawn)
+            {
+                Event?.Invoke();
+                playerTransform.position = RespawnPoint.position;
+            }
+            else
+            {
+                LanternController controller = collision.gameObject.GetComponentInChildren<LanternController>();
+                controller.TakeDamage(DamageAmount);
+            }
         }
     }
 }
