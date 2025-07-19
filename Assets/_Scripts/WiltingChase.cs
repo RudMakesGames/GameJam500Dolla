d98f0fc38071c41bef42fa0cba98b81d375cf6a2
Rudraneil <rudraneil123sarkar@gmail.com>
2025-07-19 05:50:58 +0530

    FinalFix

--- Assets/_Scripts/WiltingChase.cs
+++ Assets/_Scripts/WiltingChase.cs
@@ -1,10 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WiltingChase : MonoBehaviour
 {
     public float ChaseSpeed = 5;
     public Sprite WitheringSprite;
-
+    public Color32 DarkSkyColor,CloudColor;
     private void Update()
     {
         transform.position += Vector3.right * ChaseSpeed * Time.deltaTime;
@@ -19,6 +20,14 @@ public class WiltingChase : MonoBehaviour
         if(collision.gameObject.CompareTag("Flower"))
         {
             collision.gameObject.GetComponent<SpriteRenderer>().sprite = WitheringSprite;
+        }
+        if (collision.gameObject.CompareTag("Sky"))
+        {
+            collision.gameObject.GetComponent<SpriteRenderer>().color = DarkSkyColor;
+        }
+        if (collision.gameObject.CompareTag("Cloud"))
+        {
+            collision.gameObject.GetComponent<SpriteRenderer>().color = CloudColor;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
